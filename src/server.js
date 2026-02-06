136af559888676bd61b7cdcd2532a9deb141c41b
FCP2 <francisco.vicencio.mx@gmail.com>
2026-02-06 13:46:37 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 10000;  // Usa 10000 como fallback para Render
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {  // Agrega '0.0.0.0' como host
-  console.log(`Servidor escuchando en puerto ${PORT}`);  // Quita 'localhost' del log
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
