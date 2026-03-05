0fee74cb31db978110965c3ddc8fecef2ce7e916
FCP2 <francisco.vicencio.mx@gmail.com>
2026-03-05 17:33:12 -0600

    Update analista.routes.js

--- src/routes/analista.routes.js
+++ src/routes/analista.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/analista.controller');
 const { requireAuth, requireRole, requireOffice } = require('../middlewares/auth');
+const { applySmartFilters } = require('../middlewares/smartFilter'); // .js!
 
-router.get('/municipios/cobertura', requireAuth, requireRole('analista','superadmin'), requireOffice, ctrl.coberturaMunicipios);
+router.get('/municipios/cobertura', requireAuth, requireRole('analista','superadmin'), requireOffice, applySmartFilters, ctrl.coberturaMunicipios);
 router.get('/personas',            requireAuth, requireRole('analista','superadmin'), requireOffice, ctrl.personasPorMunicipio);
 
 module.exports = router;
