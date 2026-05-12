459e829fb4cb45ba23081238f7022c37b6d51f81
FCP2 <francisco.vicencio.mx@gmail.com>
2026-05-12 15:38:01 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -58,4 +58,9 @@ app.get('/api-status', (req, res) => {
   res.send('API funcionando correctamente 🚀');
 });
 
+// Health check (Render)
+app.get('/health', (req, res) => {
+  res.status(200).send('ok');
+});
+
 module.exports = app;
