0044a9549c4c520730b32f739fecf3b1c9fbfb92
FCP2 <francisco.vicencio.mx@gmail.com>
2026-05-21 15:38:12 -0600

    Add new route for capasMapaMunicipios

--- src/routes/analista.routes.js
+++ src/routes/analista.routes.js
@@ -4,6 +4,7 @@ const { requireAuth, requireRole, requireOffice } = require('../middlewares/auth
 const { applySmartFilters } = require('../middlewares/smartFilter'); // .js!
 
 router.get('/municipios/cobertura', requireAuth, requireRole('analista','superadmin'), requireOffice, applySmartFilters, ctrl.coberturaMunicipios);
+router.get('/municipios/capas',    requireAuth, requireRole('analista','superadmin'), requireOffice, applySmartFilters, ctrl.capasMapaMunicipios);
 router.get('/personas',            requireAuth, requireRole('analista','superadmin'), requireOffice, ctrl.personasPorMunicipio);
 
 module.exports = router;
